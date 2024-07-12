136e9bdf343962b901c625d16b67c79c57464f45
ibdkn <ibudiakina@gmail.com>
2024-07-12 21:24:50 +0300

    added controller for update user

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -45,10 +45,10 @@ class UserController extends Controller
     // Функция actionView: используется для отображения страницы с подробной информацией о конкретной модели.
     public function actionView($id)
     {
-        // // Находит модель по переданному идентификатору
+        // Получение модели по идентификатору ($id):
         $model = $this->findModel($id);
 
-        // // Возвращает представление 'view' и передает в него найденную модель
+        // Возвращает представление 'view' и передает в него найденную модель
         return $this->render('view', [
             'model' => $model,
         ]);
@@ -56,11 +56,35 @@ class UserController extends Controller
 
     public function actionUpdate($id)
     {
+        // Получение модели по идентификатору ($id):
         $model = $this->findModel($id);
+
+        // Загрузка данных и сохранение модели:
+
+        //  - load загружает данные из POST-запроса в модель. Если данные были успешно загружены, возвращается true
+        //	- save сохраняет модель в базе данных. Если сохранение прошло успешно, возвращается true
+        //	- Весь условный оператор проверяет, были ли данные успешно загружены и сохранены
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
+
+            // Если данные были успешно загружены и сохранены, происходит перенаправление пользователя на страницу
+            // index. Метод redirect создает HTTP-заголовок для перенаправления на указанное действие
             return $this->redirect(['index']);
         }
         return $this->render('update', [
+
+            // Если данные не были загружены или сохранены, метод render отображает представление update, передавая в
+            // него модель User как переменную model. Это позволяет пользователю увидеть форму обновления и, при необходимости, повторить попытку
+            'model' => $model,
+        ]);
+    }
+
+    public function actionCreate()
+    {
+        $model = new User();
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['index']);
+        }
+        return $this->render('create', [
             'model' => $model,
         ]);
     }
